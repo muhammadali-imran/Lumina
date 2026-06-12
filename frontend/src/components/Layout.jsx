0f1d66073016039799807afe56439a037eac3fb2
muhammadali-imran <imranmuhammadali633@gmail.com>
2026-06-12 22:33:28 +0500

    Fix: The named import problem for footer to Footer

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import Header from './header/Header'
-import Footer from './footer/footer'
+import Footer from './footer/Footer'
 
 export default function Layout({ children }) {
   return (
